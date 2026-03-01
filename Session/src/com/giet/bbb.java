4ac48be5a49645eea0142c4c71f4bd59b56cbfb6
Sangram Rath <sangramrath0001@gmail.com>
2026-03-01 17:09:00 +0530

    Implement HttpServlet to handle GET requests

--- Session/src/com/giet/bbb.java
+++ Session/src/com/giet/bbb.java
@@ -1,4 +1,28 @@
 package com.giet;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+public class bbb extends HttpServlet {
 
-public class bbb {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String name = request.getParameter("name");
+        String city = request.getParameter("city");
+
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+        pw.println("<a href='/NewProject1/servletc? name="+name+"&city="+city+"'>Save</a>");
+        pw.println("<html>");
+        pw.println("<head><title>Result</title></head>");
+        pw.println("<body>");
+
+
+        pw.println("<h2>Submitted Details</h2>");
+        pw.println("<h3>Name: " + name + "</h3>");
+        pw.println("<h3>City: " + city + "</h3>");
+
+        pw.println("</body>");
+        pw.println("</html>");
+    }
 }
